7f8aae4dbcac1f3fbba2450fa3aad6318a4c700c
NeelkanthSingh <neelkanthsingh.jr@gmail.com>
2024-04-14 22:07:40 +0530

    Added logger, fixed problem with credentials.js

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
-const express = require('express')
-require('dotenv').config()
-const cors = require('cors')
-const corsOptions = require('./config/corsOption')
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const corsOptions = require('./config/corsOption');
+const credentials = require('./middleware/credentials');
+const { logger } = require('./middleware/logEvents');
 
 const app = express()
 const PORT = process.env.PORT || 3000;
 
+app.use(logger);
 app.use(credentials);
 app.use(cors(corsOptions));
 
